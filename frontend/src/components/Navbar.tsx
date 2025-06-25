2d763dc739375cb16b5f1d18b31ed38c8d3ffd3e
IshaanShinde <ishaanshinde08@gmail.com>
2025-06-25 12:26:44 -0800

    scrollbar hide + page resize fix

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="buttonGroup">
+            <Link className='button' to='/'>Home</Link>
             <Link className='button' to='/A1'>A1</Link>
             <Link className='button' to='/A2'>A2</Link>
-            <Link className='button' to='/A3'>A3</Link>
+            {/* <Link className='button' to='/A3'>A3</Link>
             <Link className='button' to='/A4'>A4</Link>
-            <Link className='button' to='/A5'>A5</Link>
+            <Link className='button' to='/A5'>A5</Link> */}
         </div>
     );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,6 +13,23 @@ const Home = () => {
             <Link className='subComponent' to='/A4'>Assignment 4</Link>
             <Link className='subComponent' to='/A5'>Assignment 5</Link>
 
+            <Link className='subComponent' to='/A1'>Assignment 1</Link>
+            <Link className='subComponent' to='/A2'>Assignment 2</Link>
+            <Link className='subComponent' to='/A3'>Assignment 3</Link>
+            <Link className='subComponent' to='/A4'>Assignment 4</Link>
+            <Link className='subComponent' to='/A5'>Assignment 5</Link>
+            <Link className='subComponent' to='/A1'>Assignment 1</Link>
+            <Link className='subComponent' to='/A2'>Assignment 2</Link>
+            <Link className='subComponent' to='/A3'>Assignment 3</Link>
+            <Link className='subComponent' to='/A4'>Assignment 4</Link>
+            <Link className='subComponent' to='/A5'>Assignment 5</Link>
+            <Link className='subComponent' to='/A1'>Assignment 1</Link>
+            <Link className='subComponent' to='/A2'>Assignment 2</Link>
+            <Link className='subComponent' to='/A3'>Assignment 3</Link>
+            <Link className='subComponent' to='/A4'>Assignment 4</Link>
+            <Link className='subComponent' to='/A5'>Assignment 5</Link>
+
+
         </>
     );
 };
