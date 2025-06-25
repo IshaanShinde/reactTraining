0487c59bb9791dbb36b191c29b51a0eb6d5ca6d5
IshaanShinde <ishaanshinde08@gmail.com>
2025-06-25 15:52:16 -0800

    formik implementation complete

--- frontend/src/components/A4Form.tsx
+++ frontend/src/components/A4Form.tsx
@@ -1,19 +1,42 @@
+import { useFormik } from "formik";
 
-
+interface A4FormValues {
+    firstName: string;
+    lastName: string;
+    role: string;
+}
 
 const A4Form = () => {
+
+    const formik = useFormik<A4FormValues> ({ 
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            role: ''
+        },
+        onSubmit: (values) => {
+            console.log(values)
+        }
+    });
+
     return (
         <div className="component">
-            <form>
-                    
+            <form id='A4Form' onSubmit={formik.handleSubmit}>
+
                     <label>First Name</label>
-                    <input type='text' id='firstName' name='firstName' placeholder='First Name'/>
+                    <input type='text' id='firstName' name='firstName' placeholder='First Name' 
+                    onChange={formik.handleChange} 
+                    value={formik.values.firstName}/>
 
                     <label>Last Name</label>
-                    <input type='text' id='lastName' name='lastName' placeholder='Last Name'/>
+                    <input type='text' id='lastName' name='lastName' placeholder='Last Name' 
+                    onChange={formik.handleChange} 
+                    value={formik.values.lastName}/>
                     
                     <label>Role</label>
-                    <select id='role' name='role'>
+                    <select id='role' name='role' 
+                    onChange={formik.handleChange} 
+                    value={formik.values.role}>
                         <option value='Owner'>Owner</option>
                         <option value='User'>User</option>
                         <option value='Admin'>Admin</option>
@@ -22,7 +45,7 @@ const A4Form = () => {
             </form>
 
             <div className="buttonGroup">
-                <button className="button">Submit</button>
+                <button className="button" type='submit' form='A4Form'>Submit</button>
             </div>
         </div>
     );
