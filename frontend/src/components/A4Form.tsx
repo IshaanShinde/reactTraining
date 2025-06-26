5037eee154d6816a368532eb791e3a15ebb87018
IshaanShinde <ishaanshinde08@gmail.com>
2025-06-25 17:10:38 -0800

    validation logic implemented

--- frontend/src/components/A4Form.tsx
+++ frontend/src/components/A4Form.tsx
@@ -6,17 +6,37 @@ interface A4FormValues {
     role: string;
 }
 
+interface A4FormErrors {
+    firstName?: string;
+    lastName?: string;
+    role?: string;
+}
+
 const A4Form = () => {
 
+    const initialValues = {
+        firstName: '',
+        lastName: '',
+        role: ''
+    }
+    const onSubmit = (values: A4FormValues) => {
+        console.log(values)
+    }
+    const validate = (values: A4FormValues) => {
+        let errors: A4FormErrors = {}
+
+        if (!values.firstName) { errors.firstName = 'First Name Required'}
+        if (!values.lastName) { errors.lastName = 'Last Name Required'}
+        if (!values.role) { errors.role = 'Role Required'}
+
+        return errors
+    }
+
     const formik = useFormik<A4FormValues> ({ 
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            role: ''
-        },
-        onSubmit: (values) => {
-            console.log(values)
-        }
+        initialValues,
+        onSubmit,
+        validate,
+        validateOnChange: false
     });
 
     return (
@@ -24,19 +44,23 @@ const A4Form = () => {
             <form id='A4Form' onSubmit={formik.handleSubmit}>
 
                     <label>First Name</label>
+                    { formik.errors.firstName ? <div className="error">{formik.errors.firstName}</div> : null }
                     <input type='text' id='firstName' name='firstName' placeholder='First Name' 
                     onChange={formik.handleChange} 
                     value={formik.values.firstName}/>
 
-                    <label>Last Name</label>
+                    <label>Last Name{ formik.errors.lastName ? <div className="error">{formik.errors.lastName}</div> : null }</label>
+                    
                     <input type='text' id='lastName' name='lastName' placeholder='Last Name' 
                     onChange={formik.handleChange} 
                     value={formik.values.lastName}/>
                     
                     <label>Role</label>
+                    { formik.errors.role ? <div className="error">{formik.errors.role}</div> : null }
                     <select id='role' name='role' 
                     onChange={formik.handleChange} 
                     value={formik.values.role}>
+                        <option value=''>Select</option>
                         <option value='Owner'>Owner</option>
                         <option value='User'>User</option>
                         <option value='Admin'>Admin</option>
